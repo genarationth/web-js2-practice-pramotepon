11251a1e0e818eb279abe743ef4607aac4187eda
Zeekerz <pramote.p.on@gmail.com>
2023-03-10 23:10:32 +0700

    New contender Challenge 1 Create an Age Calculator

---
+++ new_contender.js
@@ -0,0 +1,13 @@
+// Challenge 1: Create an Age Calculator
+function createAnAgeCalculator(){
+    // Birth year
+    let birthYear = 1988;
+    // Future Year
+    let futureYear = 2026;
+    // Age now
+    let yourAge = futureYear - birthYear;
+    // output Age now
+    console.log(yourAge); // 38
+}
+// Call function createAnAgeCalculator
+createAnAgeCalculator();
